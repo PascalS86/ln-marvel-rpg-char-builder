d18211037f74c1375b779e83b0aef2a800a9516e
Pascal S <pascal.senjic@gmail.com>
2020-06-22 22:36:50 +0200

    Ability

---
+++ src/ViewModels/Ability.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace csandra.ln.marvel.rpg{
+    /// <summary>
+    /// Represent an ability
+    /// Ability have a base value.
+    /// Points worth to the base value can be spent to abilities of the same attribute class
+    /// </summary>
+    public class Ability{
+        /// <summary>
+        /// the name of the ability
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// the attribute of the ability
+        /// </summary>
+        public string Attribute { get; set; }
+        /// <summary>
+        /// minimum value of the ability, based on attribute
+        /// </summary>
+        public int BaseValue{get;set;}
+        /// <summary>
+        /// value of the ability
+        /// </summary>
+        public int Value{get;set;}
+    }
+}
